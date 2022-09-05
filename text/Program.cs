ac9e17c177549ab0cfcc1b73d29b44dc84fd5f67
zqf <1936534366@qq.com>
2022-09-05 16:58:40 +0800

    16点58分

--- text/Program.cs
+++ text/Program.cs
@@ -2,18 +2,21 @@
 Console.Read();
 async Task t1()
 {
+    Console.WriteLine("t1");
     var task = t2(3,4);
     int answer = await task;
     Console.WriteLine(answer);
 }
 async Task<int> t2(int i,int j)
 {
+    Console.WriteLine("t2");
     var task = t3(i,j);
     int answer = await task;
     return answer;
 }
 async Task<int> t3(int i,int j)
 {
+    Console.WriteLine("t3");
     var task = Task.Delay(1000);
     await task;
     int answer = i * j;
