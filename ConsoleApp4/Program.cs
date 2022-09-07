91cb382323d6a757c733937c813ef779814ff7ad
zqf <1936534366@qq.com>
2022-09-07 10:20:22 +0800

    2022年9月7日，10点20分

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+async Task<int> text(int i)
+{
+    int num = 0;
+    await Task.Run(() =>
+    {
+        for (int j = 0; j < i; j++)
+        {
+            Thread.Sleep(500);
+            Console.WriteLine("i:"+num+" id:"+Thread.CurrentThread.ManagedThreadId);
+            num =+ j;
+        }
+    });
+    return num;
+}
+
+text(5);
+while (true)
+{
+    Thread.Sleep(1000);
+    Console.WriteLine("_______");
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+var task = printAsync();
+
+for (int i = 0; i < 10; i++)
+{
+    Thread.Sleep(1000);
+    Console.WriteLine("当前数字main"+i);
+}
+
+int answer = await task;
+Console.WriteLine(answer);
+
+
+
+
+async Task<int> printAsync()
+{
+    
+    var task = Task.Run(() =>
+    {
+        int j = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            Thread.Sleep(1000);
+            j += i;
+            Console.WriteLine("当前数字print"+i);
+        }
+
+        return j;
+    });
+    int answer = await task;
+    return answer;
+}
+
+Console.ReadLine();
