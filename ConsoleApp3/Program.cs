de97a8abd48419ebd7e1ec75224abfea30e3d91d
zqf <1936534366@qq.com>
2022-09-07 10:19:11 +0800

    2022年9月7日，10点19分

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+for (int i = 0; i < 10; i++)
+{
+    Thread.Sleep(1000);
+    Console.WriteLine("a" + i);
+}
+Console.WriteLine("read");
+//var task = wait();
+await wait();
+Console.WriteLine("ok");
+for (int i = 0; i < 10; i++)
+{
+    Thread.Sleep(1000);
+    Console.WriteLine("b" + i);
+}
+
+//await task;
+
+
+
+async Task wait()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine("c"+i);
+    }
+}
+
+/*
+Console.WriteLine(await print());
+async Task<int> print()
+{
+    
+    
+    Thread.Sleep(1000);
+    var task = num();
+    Console.WriteLine("this=" + task);
+    
+    int rt = await task;
+
+    
+    return rt;
+}
+
+async Task<int> num()
+{
+    var task = Task.Delay(1000);
+    await task;
+    return 100;
+}*/
--- text/Program.cs
+++ text/Program.cs
@@ -2,6 +2,7 @@
 Console.Read();
 async Task t1()
 {
+    Thread.Sleep(4000);
     Console.WriteLine("t1");
     var task = t2(3,4);
     int answer = await task;
@@ -9,16 +10,19 @@ async Task t1()
 }
 async Task<int> t2(int i,int j)
 {
+    Thread.Sleep(4000);
     Console.WriteLine("t2");
     var task = t3(i,j);
     int answer = await task;
     return answer;
+    
 }
 async Task<int> t3(int i,int j)
 {
-    Console.WriteLine("t3");
-    var task = Task.Delay(1000);
-    await task;
+    Thread.Sleep(4000);
     int answer = i * j;
+    Console.WriteLine("t3");
+    var task = Task.Delay(10000);
+    await task;
     return answer;
 }
